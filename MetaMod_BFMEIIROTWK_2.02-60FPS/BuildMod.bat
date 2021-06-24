bf3d4e1d5cc383ea2f2ab08226c98451060e1edc
MetaIdea <26028969+MetaIdea@users.noreply.github.com>
2021-06-24 02:55:10 +0200

    2.02 versions

---
+++ MetaMod_BFMEIIROTWK_2.02-60FPS/BuildMod.bat
@@ -0,0 +1,3 @@
+bigtool_x86_64_win.exe -c "!!!_BFMEII_ROTWK_METAMOD.big" data art maps libraries lotr.csf
+xcopy /y /h /v /i "!!!_BFMEII_ROTWK_METAMOD.big" "D:\Spiele\BFME\BFMEII ROWTK 2.02\"
+pause
